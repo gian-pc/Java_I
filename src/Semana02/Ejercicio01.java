68f8fe08b2f5a604dc023ef974d7fe164e37214c
Gian Paucar <gpaucarcortez@gmail.com>
2023-03-29 12:49:51 -0500

    Semana02/ejercicio01

--- src/Semana02/Ejercicio01.java
+++ src/Semana02/Ejercicio01.java
@@ -1,7 +1,23 @@
 package Semana02;
 
+// 1. Dados dos números, que determine la suma, resta, multiplicación, división, potencia
+
+import java.util.Scanner;
+
 public class Ejercicio01 {
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese número 1: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Ingrese número 2: ");
+        int number2 = sc.nextInt();
+
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
+        System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));
+        System.out.println(number1 + " Pot " + number2 + " = " + (Math.pow(number1,number2)));
     }
 }
